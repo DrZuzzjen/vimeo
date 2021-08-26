24a094250b29dd58446eacc142da98adbda94534
DrZuzzjen <gutijeanf@gmail.com>
2021-08-26 10:48:28 +0200

    Ya conectamos con la api y obtenemos token

--- src/app/rutina/rutina.component.ts
+++ src/app/rutina/rutina.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Ejercicios, Preferencias } from './rutina.interface';
 import { rutinas } from './rutinas';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-rutina',
@@ -20,24 +22,60 @@ export class RutinaComponent {
     numero: 1,
     modelo: '',
     codigo: '1',
- 
+
   };
   //Buscar datos preferencias de usuario
   buscarRutina() {
     //Buscar datos preferencias de usuario dentro de las rutinas
-    const preferenciaZona :string | any = this.preferencias.zona;
-    const preferenciaNivel :number | any = this.preferencias.numero;
-    
-    const found = rutinas.filter(item => item.zona.indexOf(preferenciaZona) !== -1 )
-    const nivelOk = found.filter(item => item.numero.toString().indexOf(preferenciaNivel) !== -1 )
-    
-      console.log('Se encontraron: ',found.length ,' rutinas')
-      console.log(found)
-      console.log(nivelOk)
-      
+    const preferenciaZona: string | any = this.preferencias.zona;
+    const preferenciaNivel: number | any = this.preferencias.numero;
+    //Filtar por zona
+    const found = rutinas.filter(item => item.zona.indexOf(preferenciaZona) !== -1)
+    //Filtar resultado por nivel
+    const nivelOk = found.filter(item => item.numero.toString().indexOf(preferenciaNivel) !== -1)
+    //Extraer codigos de playlist
+    const playlistCode = nivelOk.map(item => item.codigo)
+    console.log('Preferencia de Usuario: ' ,this.preferencias.zona , '/ Nivel: ' , this.preferencias.numero )
+    console.log('Se encontraron: ', found.length, ' rutinas con la zona', preferenciaZona)
+    console.log(found)
+    console.log(`Se encontraron ${nivelOk.length} rutinas ${preferenciaZona} con el nivel ${preferenciaNivel}` )
+    playlistCode.forEach(item => console.log('Video: ', item))
+
+    return playlistCode
 
 
-     
+  }
+
+  generarPlaylist() {
+
+  }
+
+  conectarseAVimeo() {
+
+    const token = '11a90bb6da5f1066dc82249cb8ad468f'
+
+    const configUrl = {
+      headers: new HttpHeaders({ 
+
+        'Authorization': 'Basic MDJiYjlhNWVmYjBhYTdkMzcxNDQxYzI2YzdjMjhlYjI2MzM2MGY5MzpRT0xkZlluZFBOcVZrZkxQcmgxL3BoSUYyNFZxQlJJeDZmVXJGY3FoUFlCV2xiaXNmVk43S2Q4MzluN3J2TytYQW1VRmYyblZYS1p5NjJtNHZLdThRb3BwSWxIaStJaitBWWNWMjR1OWhxY283cGEwaXg4aTFIMWZHVS9OdXFqeQ==',
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.vimeo.*+json;version=3.4'
+      })
     }
     
+    const body = {
+      'grant_type': 'client_credentials',
+      'scope': 'public'
+    };
+    const url = 'https://api.vimeo.com/oauth/authorize/client'
+
+    //Metodo post para pedir token
+    this.http.post<any>(url, body, configUrl)
+    .subscribe(data => {
+        console.log(data)
+    })
   }
+  //Inyectamos el servicio
+  constructor(private http: HttpClient) { }
+  
+}
